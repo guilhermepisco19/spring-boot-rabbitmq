8ac73eeb6c846547e7ea66ebfdf11458046777da
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2021-01-30 17:58:21 +0000

    Creating Consumer project and configurations

---
+++ spring-consumer/src/main/java/com/example/springproducer/dto/Message.java
@@ -0,0 +1,20 @@
+package com.example.springproducer.dto;
+
+public class Message {
+
+	String text;
+
+	public Message() {
+		
+	}
+	
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
